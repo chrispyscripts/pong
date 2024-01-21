bee1239677de2c5380eee015534efe7096a6a9b8
Chris Harder <Chrisjayharder@gmail.com>
2024-01-20 23:56:18 -0700

    create paddle class and initiate 2 paddles

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 screen.listen()
-# screen.onkey(paddle1.up, "Up")
-# screen.onkey(paddle1.down, "Down")
-# screen.onkey(paddle2.up, "W")
-# screen.onkey(paddle2.up, "S")
+
+paddle1 = Paddle(1)
+paddle2 = Paddle(-1)
+
+screen.onkey(paddle1.up, "Up")
+screen.onkey(paddle1.down, "Down")
+screen.onkey(paddle2.up, "w")
+screen.onkey(paddle2.down, "s")
+
+game_on = True
+
+while game_on:
+    screen.update()
+
 screen.exitonclick()
 
-paddle1 = Paddle()
--- paddle.py
+++ paddle.py
@@ -1,20 +1,27 @@
 from turtle import Turtle
-STARTING_POSITION = [(350, -40), (350, -20), (350, -00), (350, -20), (350, 40)]
+
 
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, side):
         super().__init__()
-        self.segments = []
-        self.initialize(1)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto((350 * side), 0)
 
-    def initialize(self, side):
-        for segment in STARTING_POSITION:
-            new_segment = Turtle()
-            new_segment.shape("square")
-            new_segment.penup()
-            new_segment.color("white")
-            new_segment.goto(segment)
-            self.segments.append(new_segment)
-            side = side
+    def up(self):
+        new_ycor = self.ycor() + 20
+        self.goto(self.xcor(), new_ycor)
+
+    def down(self):
+        new_ycor = self.ycor() - 20
+        self.goto(self.xcor(), new_ycor 
+
+
+
+
+
+
